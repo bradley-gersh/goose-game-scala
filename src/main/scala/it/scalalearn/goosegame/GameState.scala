96d9bda6aaf801b49025c456336527f4673373c6
Bradley Gersh <bradley.gersh@bitrock.it>
2022-06-21 17:45:28 +0200

    feat: players add

--- src/main/scala/it/scalalearn/goosegame/GameState.scala
+++ src/main/scala/it/scalalearn/goosegame/GameState.scala
@@ -1,14 +1,27 @@
 package it.scalalearn.goosegame
 
-class GameState(val players: List[Player]) {
-    def addPlayer(newPlayer: Player) = {
-        val newPlayers = players :+ newPlayer
-        println("players: " + newPlayers.map(_.name))
-        new GameState(newPlayers)
+class GameState(val players: List[Player], val status: String = "") {
+    def getPlayers: List[Player] = players
+
+    def addPlayer(newPlayer: Player): GameState = {
+        if (players.exists(_.name == newPlayer.name)) {
+            setStatus(s"${newPlayer.name}: already existing player")
+        } else {
+            val newPlayers = players :+ newPlayer
+            new GameState(newPlayers, s"players: ${newPlayers.map(_.name).mkString(", ")}")
+        }
     }
+    
+    def addPlayer(newPlayerName: String): GameState =
+        addPlayer(Player(newPlayerName, 0))
+
+    def setStatus(newStatus: String): GameState =
+        new GameState(players, newStatus)
+        
 }
 
 object GameState {
     def apply() = new GameState(List[Player]())
     def apply(players: List[Player]) = new GameState(players)
+    def apply(players: List[Player], status: String) = new GameState(players, status) 
 }
--- src/main/scala/it/scalalearn/goosegame/main.scala
+++ src/main/scala/it/scalalearn/goosegame/main.scala
@@ -1,7 +1,39 @@
 package it.scalalearn.goosegame
 
-object Main {    
+import scala.io.StdIn.readLine
+
+object Main {
     def main(args: Array[String]): Unit = {
-        
+        println(banner)
+        cli(GameState())
+    }
+
+    val banner = """
+        |  Goose Game
+        |
+        |  To add player Pippo: "add player Pippo"
+        |  To move player Pippo: "move Pippo"
+        |  Enter blank line to exit.
+        |
+        |""".stripMargin
+
+    def cli(gameState: GameState): Unit = {
+        Option(readLine("> ")) match {
+            case Some("") | None => println("goodbye\n")
+            case Some(input) => val newGameState = processInput(input, gameState)
+                println(newGameState.status + "\n")
+                cli(newGameState)
+        }
+    }
+
+    def processInput(input: String, gameState: GameState): GameState = {
+        if (input.isEmpty) {
+            gameState.setStatus("no input")
+        } else input.split(" ") match {
+                case Array("add", "player", newName) => {
+                    gameState.addPlayer(newName)
+                }
+                case _ => gameState.setStatus("unrecognized command")
+            }
     }
 }

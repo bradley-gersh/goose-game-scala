8c931c1de7b81647de44047208a49ae60f89301d
Bradley Gersh <bradley.gersh@bitrock.it>
2022-06-29 17:53:58 +0200

    refactor: refactored out dice functions

--- src/main/scala/it/scalalearn/goosegame/Main.scala
+++ src/main/scala/it/scalalearn/goosegame/Main.scala
@@ -41,21 +41,28 @@ object Main {
     } else input.split(" ") match {
         case Array("add", "player", newName) => Logic.addPlayer(gameState, newName)
         case Array("move", name, die1String, die2String) =>
-          val die1 =
-            if (die1String.last == ',') die1String.init.toInt
-            else die1String.toInt
-          val die2 = die2String.toInt
-          
+          val (die1, die2) = diceStringsToInt(die1String, die2String)
           Logic.movePlayer(gameState, name, die1, die2)
 
         case Array("move", name) =>
-          val random = new Random(System.nanoTime())
-          val die1 = random.nextInt(6) + 1
-          val die2 = random.nextInt(6) + 1
-          
+          val (die1, die2) = randomDice2()
           Logic.movePlayer(gameState, name, die1, die2)
 
         case _ => Left("unrecognized command")
       }
+  }
+
+  private def diceStringsToInt(die1String: String, die2String: String): (Int, Int) = {
+    val die1 =
+      if (die1String.last == ',') die1String.init.toInt
+      else die1String.toInt
+    val die2 = die2String.toInt
+
+    (die1, die2)
+  }
+
+  private def randomDice2(): (Int, Int) = {
+    val random = new Random(System.nanoTime())
+    (random.nextInt(6) + 1, random.nextInt(6) + 1)
   }
 }

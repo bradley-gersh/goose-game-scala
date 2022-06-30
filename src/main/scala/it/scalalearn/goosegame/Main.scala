eff9cf21ac0d419a47c37a95151bc12748aedbc7
Bradley Gersh <bradley.gersh@bitrock.it>
2022-06-30 10:02:01 +0200

    refactor: processed input with regex

--- src/main/scala/it/scalalearn/goosegame/Main.scala
+++ src/main/scala/it/scalalearn/goosegame/Main.scala
@@ -5,19 +5,24 @@ import scala.io.StdIn.readLine
 import scala.util.Random
 
 object Main {
+  private val ADD_PLAYER = """\s*(?i)add\s+player(?-i)\s+(\w+)""".r
+  private val MOVE_PLAYER_RANDOM_DICE = """\s*(?i)move(?-i)\s+(\w+)""".r
+  private val MOVE_PLAYER_CHOSEN_DICE = """\s*(?i)move(?-i)\s+(\w+)\s+(\d+),\s*(\d+)""".r
+  private val random = new Random(System.nanoTime())
+
+  private val banner: String = """
+                                 |  Goose Game
+                                 |
+                                 |  To add player Pippo: "add player Pippo"
+                                 |  To move player Pippo: "move Pippo"
+                                 |  Enter blank line to exit.
+                                 |
+                                 |""".stripMargin
+
   def main(args: Array[String]): Unit = {
     println(banner)
     cli(Map[String, Int]())
   }
-
-  private val banner: String = """
-    |  Goose Game
-    |
-    |  To add player Pippo: "add player Pippo"
-    |  To move player Pippo: "move Pippo"
-    |  Enter blank line to exit.
-    |
-    |""".stripMargin
 
   @tailrec
   def cli(oldGameState: Map[String, Int]): Unit = {
@@ -38,32 +43,18 @@ object Main {
   def processInput(input: String, gameState: Map[String, Int]): Either[String, (Map[String, Int], String)] = {
     if (input.isEmpty) {
       Left("no input")
-    } else input.split(" ") match {
-        case Array("add", "player", newName) => Logic.addPlayer(gameState, newName)
+    } else input match {
+        case ADD_PLAYER(newName) => Logic.addPlayer(gameState, newName)
 
-        case Array("move", name, die1String, die2String) =>
-          val (die1, die2) = diceStringsToInt(die1String, die2String)
+        case MOVE_PLAYER_CHOSEN_DICE(name, die1String, die2String) =>
+          val (die1, die2) = (die1String.toInt, die2String.toInt)
           Logic.movePlayer(gameState, name, die1, die2)
 
-        case Array("move", name) =>
-          val (die1, die2) = randomDice2()
+        case MOVE_PLAYER_RANDOM_DICE(name) =>
+          val (die1, die2) = (random.nextInt(6) + 1, random.nextInt(6) + 1)
           Logic.movePlayer(gameState, name, die1, die2)
 
         case _ => Left("unrecognized command")
       }
-  }
-
-  private def diceStringsToInt(die1String: String, die2String: String): (Int, Int) = {
-    val die1 =
-      if (die1String.last == ',') die1String.init.toInt
-      else die1String.toInt
-    val die2 = die2String.toInt
-
-    (die1, die2)
-  }
-
-  private def randomDice2(): (Int, Int) = {
-    val random = new Random(System.nanoTime())
-    (random.nextInt(6) + 1, random.nextInt(6) + 1)
   }
 }

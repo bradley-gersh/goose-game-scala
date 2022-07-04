f2a608db95542414616f27ba40c515fc04fe083f
Bradley Gersh <bradley.gersh@bitrock.it>
2022-07-04 16:08:49 +0200

    refactor: removed extra pattern matching

--- src/main/scala/it/scalalearn/goosegame/gamestate/SpecialSquares.scala
+++ src/main/scala/it/scalalearn/goosegame/gamestate/SpecialSquares.scala
@@ -3,7 +3,7 @@ package it.scalalearn.goosegame.gamestate
 object SpecialSquares {
   final val FIRST_SQUARE = 0
   final val LAST_SQUARE = 63
-  final val BRIDGE = 6
+  final val BRIDGE_SQUARE = 6
   final val BRIDGE_END = 12
   final val GOOSE_SQUARES = Set(5, 9, 14, 18, 23, 27)
 }
--- src/main/scala/it/scalalearn/goosegame/movelogic/MoveHandler.scala
+++ src/main/scala/it/scalalearn/goosegame/movelogic/MoveHandler.scala
@@ -2,13 +2,13 @@ package it.scalalearn.goosegame.movelogic
 
 import it.scalalearn.goosegame.errors.{DiceError, DoubledPlayerError, GameError, UnknownPlayerError}
 import it.scalalearn.goosegame.gamestate.GameState
-import it.scalalearn.goosegame.gamestate.SpecialSquares.{BRIDGE, BRIDGE_END, GOOSE_SQUARES, LAST_SQUARE}
+import it.scalalearn.goosegame.gamestate.SpecialSquares.{BRIDGE_SQUARE, BRIDGE_END, GOOSE_SQUARES, LAST_SQUARE}
 import it.scalalearn.goosegame.movelogic.MoveType
+import it.scalalearn.goosegame.movelogic.MoveType._
 import it.scalalearn.goosegame.rosterlogic.RosterHandler
 import it.scalalearn.goosegame.readout.{FinalReadout, ReadoutData, ReadoutBuilder}
 
 import scala.annotation.tailrec
-import scala.collection.mutable
 
 object MoveHandler {
   def movePlayer(gameState: GameState, name: String, dice: List[Int]): Either[GameError, (GameState, FinalReadout)] = {
@@ -19,45 +19,43 @@ object MoveHandler {
       val move = Move(name, startSquare, startSquare, validDice)
       val startReadout = ReadoutBuilder.logStartRoll(name, startSquare, validDice)
 
-      val squareList = makeSquareList(move, List[Int]())
-      val (newGameState, finalReadout) = interpretMoves(gameState, move, squareList, startReadout)
+      val moveList = makeMoveList(move, List[(MoveType, Int)]())
+      val (newGameState, finalReadout) = interpretMoves(gameState, move, moveList, startReadout)
 
       (newGameState, finalReadout.seal())
     }
   }
 
   @tailrec
-  def makeSquareList(firstMove: Move, squareList: List[Int]): List[Int] = {
+  def makeMoveList(firstMove: Move, squareList: List[(MoveType, Int)]): List[(MoveType, Int)] = {
     val Move(name, previousSquare, startSquare, dice) = firstMove
 
     previousSquare + dice.sum match {
-      case BRIDGE =>
-        (BRIDGE_END :: BRIDGE :: squareList).reverse
+      case LAST_SQUARE => ((LAST, LAST_SQUARE) :: squareList).reverse
+
+      case BRIDGE_SQUARE => ((NORMAL, BRIDGE_END) :: (BRIDGE, BRIDGE_SQUARE) :: squareList).reverse
 
       case beyondLastSquare if beyondLastSquare > LAST_SQUARE =>
         val postBounceSquare = LAST_SQUARE - (beyondLastSquare - LAST_SQUARE)
-        (postBounceSquare :: beyondLastSquare :: squareList).reverse
+        ((NORMAL, postBounceSquare) :: (BOUNCE, beyondLastSquare) :: squareList).reverse
 
       case gooseSquare if GOOSE_SQUARES(gooseSquare) =>
-        val nextMove = Move(name, gooseSquare, startSquare, dice)
-        makeSquareList(nextMove, gooseSquare :: squareList)
+        makeMoveList(Move(name, gooseSquare, startSquare, dice), (GOOSE_START, gooseSquare) :: squareList)
 
-      case normalSquare =>
-        (normalSquare :: squareList).reverse
+      case normalSquare => ((NORMAL, normalSquare) :: squareList).reverse
     }
   }
 
-  def interpretMoves(gameState: GameState, move: Move, squareList: List[Int], readoutData: ReadoutData): (GameState, ReadoutData) = {
+  def interpretMoves(gameState: GameState, move: Move, squareList: List[(MoveType, Int)], readoutData: ReadoutData): (GameState, ReadoutData) = {
     val Move(name, _, startSquare, dice) = move
 
     assert(validateDice(dice).isRight)
     assert(gameState.hasPlayer(name))
 
     squareList.foldLeft((gameState, readoutData))(
-      (stateTuple, square) => {
+      (stateTuple, squareTuple) => {
         val (newGameState, newReadoutData) = stateTuple
-
-        val moveType = getMoveType(square)
+        val (moveType, square) = squareTuple
 
         val updatedReadout = ReadoutBuilder.appendMove(newReadoutData, name, square, moveType)
         val (prankGameState, prankReadoutData) = checkPrank(newGameState, name, square, startSquare, updatedReadout)
@@ -93,12 +91,4 @@ object MoveHandler {
       Right(dice)
     else
       Left(DiceError)
-
-  def getMoveType(square: Int): MoveType = square match {
-    case LAST_SQUARE => MoveType.LAST
-    case BRIDGE => MoveType.BRIDGE
-    case beyondLastSquare if beyondLastSquare > LAST_SQUARE => MoveType.BOUNCE
-    case gooseSquare if GOOSE_SQUARES(gooseSquare) => MoveType.GOOSE_START
-    case _ => MoveType.NORMAL
-  }
 }

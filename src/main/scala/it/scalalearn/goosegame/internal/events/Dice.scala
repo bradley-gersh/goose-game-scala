9064a59d5f3baca7c990676f5b7a7c586c28318d
Bradley Gersh <bradley.gersh@bitrock.it>
2022-07-08 12:18:55 +0200

    refactor: dice upgraded to case class

--- src/main/scala/it/scalalearn/goosegame/internal/events/Dice.scala
+++ src/main/scala/it/scalalearn/goosegame/internal/events/Dice.scala
@@ -2,11 +2,11 @@ package it.scalalearn.goosegame.internal.events
 
 import it.scalalearn.goosegame.ui.errors.{GameError, DiceError}
 
-class Dice(val values: List[Int]) extends AnyVal {
+case class Dice(values: List[Int]) extends AnyVal {
   def sum: Int = values.sum
 }
 
-object Dice {
+case object Dice {
   def makeDice(dice: Int*): Either[GameError, Dice] =
     if (dice.forall(die => die >= 1 && die <= 6))
       Right(Dice(dice.toList))

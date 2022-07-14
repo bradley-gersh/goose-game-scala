519c8c4408b634195b1754a5aa4e20a9305628f7
Bradley Gersh <bradley.gersh@bitrock.it>
2022-07-14 11:45:20 +0200

    fix: repaired validation that player isn't doubled

--- src/main/scala/it/scalalearn/goosegame/internal/events/RosterEventWriter.scala
+++ src/main/scala/it/scalalearn/goosegame/internal/events/RosterEventWriter.scala
@@ -1,8 +1,12 @@
 package it.scalalearn.goosegame.internal.events
 
 import it.scalalearn.goosegame.internal.gamestate.GameState
-import it.scalalearn.goosegame.ui.errors.GameError
+import it.scalalearn.goosegame.ui.errors.{DoubledPlayerError, GameError}
 
 object RosterEventWriter {
-  def addPlayer(name: String): Either[GameError, List[Event]] = Right(List(PlayerAdded(name)))
+  def addPlayer(gameState: GameState, name: String): Either[GameError, List[Event]] =
+    if (gameState.hasPlayer(name))
+      Left(DoubledPlayerError(name))
+    else
+      Right(List(PlayerAdded(name)))
 }
